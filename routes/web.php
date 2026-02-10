43bb9d7a3c2713d46505203403e8bce17659fa53
Ricardo_Chab <chabricardo6@gmail.com>
2026-02-10 00:48:50 -0600

    correccion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\EcoSazonController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('vista_inicio');
+// Establecemos la vista 'empresa' como la principal
+Route::get('/', [HomeController::class, 'empresa'])->name('home');
 
-Route::get('/contact', function(){
-    $nombre = "Esmeralda Palomino";
-    return view('contact', ['nombre'=>$nombre, 'carrera'=>'LATI']);
+// Rutas funcionales para los botones
+Route::get('/menu', [EcoSazonController::class, 'index'])->name('menu.index');
+Route::get('/login', [EcoSazonController::class, 'login'])->name('login');
+Route::get('/register', [EcoSazonController::class, 'register'])->name('register');
+Route::get('/contacto', function() {
+    return view('contact', ['nombre' => 'Equipo EcoSazón', 'carrera' => 'Soporte']);
 })->name('contact');
-
-Route::get('/principal', function(){
-    $datos = ["titulo"=>"Tienda Virtual - Vista Principal",
-    "mensaje"=>"Bienvenido a la vista principal"];
-    return view('principal', $datos);
-})->name('principal');
-
-Route::get('/empresa', [HomeController::class, 'empresa'])->name('empresa');
